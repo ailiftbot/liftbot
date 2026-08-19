bacca440e2c49d2785539ab23a00a3a195153ef7
Soaib Choudhary <chsoaib24@gmail.com>
2026-08-19 23:48:48 +0530

    Setup: Added marketing pages URLs, base links, and home/helloworld templates

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView  # <--- YEH IMPORT ADD KIYA HAI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # --- Marketing Pages (Professional Setup) ---
+    path('', TemplateView.as_view(template_name='marketing/home.html'), name='home'),
+    path('about/', TemplateView.as_view(template_name='marketing/about.html'), name='about'),
+    path('contact/', TemplateView.as_view(template_name='marketing/contact.html'), name='contact'),
+    # ---------------------------------------------
+
+    # --- Existing App Routes ---
     path('', include('apps.accounts.urls')),
     path('', include('apps.workspaces.urls')),
     path('employees/', include('apps.employees.urls')),
